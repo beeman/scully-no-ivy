5b7ff5a546ef7fc18256036fa14ae4bc85c4a8bb
Bram Borggreve <borggreve@gmail.com>
2019-12-16 17:08:40 -0500

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {IdleMonitorService} from '@scullyio/ng-lib';
+ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+ constructor (private idle: IdleMonitorService) {
+   this.idle.idle$
+     .subscribe(res => {
+       console.log(res);
+     })
+ }
+
   title = 'scully-no-ivy';
 }
